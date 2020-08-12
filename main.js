09f1a096ee87d7617e5e279dc32b7a92f4630e3c
arissatya <arissatya.nugraha@gmail.com>
2020-08-12 22:44:53 +0700

    login log out and organization task functional

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ new Vue({
   el: "#app",
   data: {
     msg: "HELLO VUE",
-    name: "ARIS",
-    organization: "HACKTIV8",
+    name: "",
+    organization: "",
     date: "date",
     position: "welcome",
+    registerName: "",
+    registerEmail: "",
+    registerOrganization: "",
+    registerPassword: "",
+    loginEmail: "",
+    loginPassword: "",
+    alertMsg: "",
     isLogin: false,
-    backlogTasks: [
-      {
-        id: 1,
-        user: "aris",
-        content: "new app research"
-      }
-    ],
+    alertOn: false,
+    organizationTasks: [],
+    backlogTasks: [],
     productTasks: [
       {
         id: 2,
@@ -37,8 +40,65 @@ new Vue({
     ],
   },
   methods: {
+    fetchOrganizationTask() {
+      axios({
+        method: "GET",
+        url: "http://localhost:3000/tasks",
+        headers: {
+          access_token: localStorage.getItem('access_token')
+        }
+      })
+        .then(response => {
+          console.log("PANGGIL");
+          console.log(response.data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .finally(_ => {
+          console.log("");
+        })
+    },
+    fetchBacklog() {
+      this.backlogTasks = [
+        {
+          id: 1,
+          user: "aris",
+          content: "new app research"
+        },
+        {
+          id: 1,
+          user: "aris",
+          content: "market research"
+        },
+        {
+          id: 1,
+          user: "aris",
+          content: "vue research"
+        },
+        {
+          id: 1,
+          user: "aris",
+          content: "independence day"
+        }
+      ]
+    },
+    fetchProduct() {
+
+    },
+    fetchDevelopment() {
+
+    },
+    fetchDone() {
+
+    },
     deleteTask(id) {
-      console.log(id);
+      const confirmation = confirm("are you sure?")
+      if (confirmation) {
+        console.log(confirmation);
+      } else {
+        console.log(confirmation);
+      }
     },
     getTask(id) {
       console.log(id);
@@ -52,9 +112,102 @@ new Vue({
     showWelcome() {
       this.position = "welcome"
     },
+    register() {
+      axios({
+        method: 'post',
+        url: 'http://localhost:3000/register',
+        data: {
+          name: this.registerName,
+          email: this.registerEmail,
+          organization: this.registerOrganization,
+          password: this.registerPassword,
+        }
+      })
+        .then(response => {
+          this.alertOn = true
+          this.alertMsg = "REGISTER SUCCESS!"
+          setTimeout(_ => {
+            this.alertOn = false
+            this.alertMsg = ""
+          }, 3000)
+          this.showLogin()
+        })
+        .catch(err => {
+          this.alertOn = true
+          this.alertMsg = "REGISTER FAILED, EMAIL ALREADY REGISTERED"
+          setTimeout(_ => {
+            this.alertOn = false
+            this.alertMsg = ""
+          }, 3000)
+          this.showRegister()
+        })
+        .finally(_ => {
+          this.registerName = ""
+          this.registerEmail = ""
+          this.registerPassword = ""
+        })
+    },
+    login() {
+      axios({
+        method: "POST",
+        url: "http://localhost:3000/login",
+        data: {
+          email: this.loginEmail,
+          password: this.loginPassword,
+        }
+      })
+        .then(response => {
+          localStorage.setItem('access_token', response.data.access_token)
+          this.fetchOrganizationTask()
+          this.name = response.data.name
+          this.organization = response.data.organization
+          this.isLogin = true
+          this.alertOn = true
+          this.alertMsg = `Welcome to KANBAN! ${response.data.name}`
+          setTimeout(_ => {
+            this.alertOn = false
+            this.alertMsg = ""
+          }, 3000)
+          console.log('WELCOME ', response.data.name, response.data.organization);
+        })
+        .catch(err => {
+          console.log(err);
+          this.alertOn = true
+          this.alertMsg = "EMAIL/PASSWORD INVALID"
+          setTimeout(_ => {
+            this.alertOn = false
+            this.alertMsg = ""
+          }, 3000)
+        })
+        .finally(_ => {
+          this.loginEmail = ""
+          this.loginPassword = ""
+        })
+    },
+    logout() {
+      this.alertOn = true
+      this.alertMsg = "LOGGED OUT!"
+      setTimeout(_ => {
+        this.alertOn = false
+        this.alertMsg = ""
+      }, 3000)
+      localStorage.removeItem('access_token')
+      this.name = ""
+      this.organization = ""
+      this.isLogin = false
+      this.showWelcome()
+    }
   },
   created() {
     console.log('CREATED');
+    this.fetchBacklog()
+    if (localStorage.getItem('access_token')) {
+      console.log('USER LOGGED IN');
+      this.isLogin = true
+    } else {
+      console.log('LOGGED OUT');
+      this.isLogin = false
+    }
   },
   computed: {
     getDate() {
@@ -82,8 +235,17 @@ new Vue({
           day = "Saturday";
       }
       let calender = new Date().toJSON().slice(0, 10).replace(/-/g, '/')
-      let today = day + " " + calender
+      let today = day + ", " + calender
       return today
-    }
+    },
+    getTime() {
+      let d = new Date()
+      let s = d.getSeconds()
+      let m = d.getMinutes()
+      let h = d.getHours()
+      let time = h + ":" + m + ":" + s
+      return time
+    },
+
   }
 })
